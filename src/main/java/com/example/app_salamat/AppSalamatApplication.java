d7492c0f0d8cefcee2ef5c73014fd038f311bd60
J0res <jores97@bk.ru>
2024-12-04 23:49:45 +0600

    first commit

---
+++ src/main/java/com/example/app_salamat/AppSalamatApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_salamat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSalamatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSalamatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/app_salamat/AppSalamatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_salamat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSalamatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
